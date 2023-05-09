ad4cf0f46276e5620a46c8af33bb5e9ee404dec2
Rajat Nai <rajatnai49@gmail.com>
2023-05-09 16:06:26 +0530

    ball rotating and colorchanging animation

---
+++ Ball-Rotation/src/main.ts
@@ -0,0 +1,100 @@
+// in this we use threejs for rendering 3d object and use gsap for animation
+import * as THREE from "three";
+import { gsap } from "gsap";
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+//just assume scene as a movie-scene where you need all material, camera, lights
+//creating scene with threejs
+const scene = new THREE.Scene();
+//define shape of the module
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+  roughness: 0.5,
+});
+
+//mesh is a combination of shape(geometry) and material
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//define white light
+const light = new THREE.PointLight(0xffffff, 2, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+// add eventlistner so when screen resize canvas automatically change its size 
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePen = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+// continuously update scene 
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
+
+// gsap animation 
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+window.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+
+// change color animation 
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
